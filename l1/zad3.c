423f937c3be62c973679364e38ca6fe92501afbf
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-03-12 19:49:38 +0100

    chore: wsl config

--- l1/zad3.c
+++ l1/zad3.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define SKIP_EVEN 1
 
--- l1/zad3_v1.c
+++ l1/zad3_v1.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define BUFFER_SIZE 50
 
 int main(int ArgC, char **ArgV) {
