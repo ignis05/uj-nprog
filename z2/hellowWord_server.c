9c3978fca61e62bb5e596b7bf824a1b6d5c80bb0
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-03-18 08:47:43 +0100

    z2: server

--- z2/hellowWord_server.c
+++ z2/hellowWord_server.c
@@ -1,58 +1,63 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define PORT 8080
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-       
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define HELLO_MSG "Hello, world!\r\n"
+
+int main(int argc, char const *argv[]) {
+    if (argc < 2) {
+        printf("Program requires argument with port number\n");
+        return 0;
+    }
+    int port = atoi(argv[1]);
+
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
+    int server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-       
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+
+    struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-       
+    address.sin_port = htons(port);
+    int addrlen = sizeof(address);
+
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
+    if (bind(server_socket, (struct sockaddr *)&address,
+             sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
+
+    printf("Server started\n");
+
+    while (1) {
+        if (listen(server_socket, 3) < 0) {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        int socket;
+        if ((socket = accept(server_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // char buffer[1024];
+        // int bytes = read(socket, buffer, 1024);
+        // printf("%s\n", buffer);
+        send(socket, HELLO_MSG, strlen(HELLO_MSG), 0);
+        printf("Hello message sent\n");
+
+        if (close(socket)) {
+            perror("close");
+            exit(EXIT_FAILURE);
+        }
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
     return 0;
 }
