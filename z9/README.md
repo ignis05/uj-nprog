78ddaac0756457b03976bc2e25b9e475bf022593
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-05-06 09:06:02 +0200

    z9: done

---
+++ z9/README.md
@@ -0,0 +1,33 @@
+# Programowanie sieciowe - zestaw 9
+
+Grzegorz Mikołajczyk
+
+## Uruchomienie
+
+1. Potrzebny node.js zainstalowany na maszynie
+2. Instalacja pakietów przez polecenie `npm i`
+3. Uruchomienie progtamu przez polecenie `node main.js`
+
+## Wybrana strona
+
+Wybrana została strona [accuweather](https://www.accuweather.com/en/pl/krakow/274455/current-weather/274455) pokazująca aktualną pogodę dla Krakowa. <br/>
+
+Program wyciąga z niej aktualną temperturę oraz inne szczegóły w stylu wilgotoność, wiatr, ciśnienie, zachmurzenie itp. i parsuje je do pojedyńczej tablicy zawierającej obiekty z właściwościami `name` oraz `value` dla każdej wyciągniętej informacji.
+
+Przykładowe wyciągnięte iformacje:
+
+```
+[
+  { name: 'Temperature', value: '13°C' },
+  { name: 'Max UV Index', value: '2 Low' },
+  { name: 'Wind', value: 'WSW 7 km/h' },
+  { name: 'Wind Gusts', value: '7 km/h' },
+  { name: 'Humidity', value: '93%' },
+  { name: 'Indoor Humidity', value: '61% (Ideal Humidity)' },
+  { name: 'Dew Point', value: '12° C' },
+  { name: 'Pressure', value: '↔ 1022 mb' },
+  { name: 'Cloud Cover', value: '88%' },
+  { name: 'Visibility', value: '8 km' },
+  { name: 'Cloud Ceiling', value: '12200 m' }
+]
+```
--- z9/main.js
+++ z9/main.js
@@ -2,6 +2,12 @@ const process = require('process')
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+/** *
+ * @typedef {Object} DetailDataNode
+ * @property {String} name
+ * @property {String} value
+ */
+
 async function main() {
 	// request
 	let res = await axios.get('https://www.accuweather.com/en/pl/krakow/274455/current-weather/274455').catch((err) => {
@@ -14,13 +20,22 @@ async function main() {
 	// parse to dom
 	const rootNode = cheerio.load(res.data)
 
+	/** * @type {Array<DetailDataNode>}*/
+	let details = []
+
 	// details object, get temperature from separate place
-	let details = [{ name: 'Temperature', value: rootNode('div.display-temp').text().trim() }]
+	let tempElement = rootNode('div.display-temp')
+	if (!tempElement.text()) console.warn('temperature data not found')
+	else details.push({ name: 'Temperature', value: tempElement.text().trim() })
 
 	// get the rest of data
 	let detailsElement = rootNode('div.current-weather-details')
 	for (let detailNode of detailsElement.find('div.detail-item')) {
 		details.push({ name: rootNode(detailNode.children[1]).text().trim(), value: rootNode(detailNode.children[3]).text().trim() })
+	}
+
+	if (Object.keys(details).length < 11) {
+		console.log(`Warning: some data was not accessible`)
 	}
 	console.log(details)
 }
